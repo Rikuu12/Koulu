8454567fde7e0fc203495714806237941eebf5d6
Rikuu12 <98409708+Rikuu12@users.noreply.github.com>
2022-02-14 14:14:28 +0200

    Add files via upload

---
+++ tehtävä4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tehtävä4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tulostaa laskurin ja viivoituksen
+            Console.WriteLine("VALUUTTALASKURI");
+            Console.WriteLine("---------------");
+            Console.WriteLine();
+
+            // Tulostaa ja tallentaa annetun summan "summa" muuttujaan
+            Console.Write("Anna summa euroina ? ");
+            int summa = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            /*Tekee muuttujan joka laskee summan ja kertoo sen annetun kurssin mukaan. Muuttaa myös tulostamaan
+            kahden desimaalin tarkkuudella.*/
+            string dollaria = string.Format("{0,10:0.00}",summa * 1.21f);
+            string puntaa = string.Format("{0,10:0.00}",summa * 0.87f);
+
+            //Tulostaa lasketut summat
+            Console.WriteLine(dollaria + " $");
+            Console.WriteLine(puntaa + " £");
+
+        }
+    }
+}
