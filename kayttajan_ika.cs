ce577c3b1c5df0f27be1f5e1c6584ab19eb5026e
Rikuu12 <98409708+Rikuu12@users.noreply.github.com>
2022-02-17 01:48:16 +0200

    Add files via upload

---
+++ kayttajan_ika.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Kysyy käyttäjän iän ja tallentaa muuttujaan
+Console.Write("Minkä ikäinen olet ? ");
+int ika = int.Parse(Console.ReadLine());
+//Kysyy käyttäjän sukupuolen ja tallentaa muuttujaan
+Console.Write("Olet nainen vai mies (N/M) ? ");
+string sukup = Console.ReadLine();
+/*valitsee vaihtoehtojen eli iän ja sukupuolen mukaan järjestyksessä käyden minkä
+vastauksen antaa tulostukseen*/
+if (ika < 14 && sukup == "M")
+{
+    Console.Write("Mitäs poitsu ?");
+}
+else if (ika < 26 && sukup == "M")
+{
+    Console.Write("Mitäs nuori mies ?");
+}
+else if (ika < 57 && sukup == "M")
+{
+    Console.Write("Olet mies parhaassa iässä !");
+}
+else if (ika > 56 && sukup == "M")
+{
+    Console.Write("Mitäs vanha ukko ?");
+}
+else if (ika < 14 && sukup == "N")
+{
+    Console.Write("Mitäs tyttö ?");
+}
+else if (ika < 26 && sukup == "N")
+{
+    Console.Write("Mitäs nuori nainen ?");
+}
+else if (ika < 57 && sukup == "N")
+{
+    Console.Write("Olet nainen parhaassa iässä !");
+}
+else if (ika > 56 && sukup == "N")
+{
+    Console.Write("Mitäs vanha akka ?");
+}
