9993011020b9f5453340bb26f5fdd6ce36481f92
Rikuu12 <98409708+Rikuu12@users.noreply.github.com>
2022-02-28 14:05:33 +0200

    Add files via upload

---
+++ lukupyramidi.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Anna lukupyramidin kerrosten määrä ? ");
+int kerroksia = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= kerroksia; i++)
+{
+    Console.WriteLine();
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(j);
+    }
+    
+}
