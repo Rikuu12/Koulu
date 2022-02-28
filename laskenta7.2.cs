3c3c028fb8ffe24759947e4a2b5acb6a69cccabb
Rikuu12 <98409708+Rikuu12@users.noreply.github.com>
2022-02-28 11:33:46 +0200

    Add files via upload

---
+++ laskenta7.2.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Anna luku 1 ja 10 väliltä ? ");
+int luku = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+for (int i = 1; i < 6; i++)
+{
+    int kerto = i * luku;
+    int jako = i / luku;
+    int jaannos = i % luku;
+    Console.WriteLine(i + " * " + luku + " = " + kerto + "  " + i +
+        " / " + luku + " = " + jako + "  " + i + " % "
+        + luku + " = " + jaannos);
+}
